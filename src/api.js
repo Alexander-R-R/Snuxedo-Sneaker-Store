98e974420f943c051e948e921c46524ede3079c0
Alexander-R-R <a.roussev@yahoo.com>
2023-05-23 13:40:33 +0300

    firebase updates

---
+++ src/api.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ5xnjErpt-G5640oN-hsWSz6538nj8PY",
+  authDomain: "snuxedo-sneaker-store.firebaseapp.com",
+  projectId: "snuxedo-sneaker-store",
+  storageBucket: "snuxedo-sneaker-store.appspot.com",
+  messagingSenderId: "824455973930",
+  appId: "1:824455973930:web:d8efab80c5cd21fedd4753"
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/Sneakers/Sneakers.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+    Link,
+    useSearchParams,
+    useLoaderData,
+    defer,
+    Await
+} from "react-router-dom"
+
+
+export function loader() {
+
+}
+
+export default function Sneakers() {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const dataPromise = useLoaderData();
+    const typeFilter = searchParams.get("type");
+  
+    React.useEffect(() => {
+      fetch("/api/sneakers")
+        .then((res) => res.json())
+        .then((data) => setShoesData(data.shoes));
+    }, []);
+
+    const displayedShoes = typeFilter ? shoesData.filter(shoe => shoe.type === typeFilter) : shoesData
+  
+    const shoesElements = displayedShoes.map((shoe) => (
+      <div key={shoe.id} className="shoe-tile">
+        <Link to={`/sneakers/${shoe.id}`}>
+          <img src={shoe.imageUrl} />
+          <div className="van-info">
+            <h3>{shoe.name}</h3>
+            <p>
+              {shoe.price}
+              <span>€</span>
+            </p>
+          </div>
+          {/* <i className={`van-type ${shoe.type} selected`}>{shoe.type}</i> */}
+        </Link>
+      </div>
+    ));
+  
+    return (
+      <div className="sneaker-list-container">
+          <img src={SneakersBanner} className="sneakers-banner"/>
+          <div className="sneaker-list-filter-buttons">
+              <Link 
+                  to="."
+                  className="van-type all-sneakers"
+              >All Sneakers</Link>
+              <Link 
+                  to="?type=air-jordan"
+                  className="van-type air-jordan"
+              >Air Jordan</Link>
+              <Link 
+                  to="?type=nike"
+                  className="van-type nike"
+              >Nike</Link>
+              <Link 
+                  to="?type=yeezy"
+                  className="van-type yeezy"
+              >Yeezy</Link>
+              <Link 
+                  to="?type=adidas"
+                  className="van-type adidas"
+              >Adidas</Link>
+          
+          </div>
+        <div className="shoe-list">{shoesElements}</div>
+      </div>
+    );
+  
+}
+
+
+
